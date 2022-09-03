ef177d8d821f2c17d9ea633e285f5b857194d128
Paula Rodrigues <paulinhaabro@gmail.com>
2022-09-03 01:05:02 -0300

    create schema

---
+++ schemas.js
@@ -0,0 +1,13 @@
+import joi from "joi";
+
+const participantSchema = joi.object({
+  name: joi.string().required(),
+});
+
+const messageSchema = joi.object({
+  to: joi.string().required(),
+  text: joi.string().required(),
+  type: joi.string().required(),
+});
+
+export {participantSchema, messageSchema};

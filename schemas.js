a4a9fc8cc8dbc5129e0e625bf68ac585d3be8728
Paula Rodrigues <paulinhaabro@gmail.com>
2022-09-03 01:08:03 -0300

    improve schema

--- schemas.js
+++ schemas.js
@@ -7,7 +7,7 @@ const participantSchema = joi.object({
 const messageSchema = joi.object({
   to: joi.string().required(),
   text: joi.string().required(),
-  type: joi.string().required(),
+  type: joi.string().valid("message", "private_message").required(),
 });
 
 export {participantSchema, messageSchema};
